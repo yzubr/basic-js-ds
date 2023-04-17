9107c8ffe672ac712531e4b10a4d08d63826eca8
yzubr <vadzim.vinnikau@gmail.com>
2023-04-17 21:13:20 +0300

    task dine

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,26 +6,70 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.base = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    if (this.base == null) {
+      return null
+    } else return this.base;
   }
+
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.base) {
+      this.base = newNode;
+      return;
+    }
+    let currentNode = this.base;
+    while (currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.letf) {
+          currentNode.left = newNode;
+          return
+        }
+        currentNode = currentNode.left;
+      }
+      else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(node, data) {
+    if (node === null) {
+      return null;
+    } else if (data < node.data) {
+      return this.search(node.left, data);
+    } else if (data > node.data) {
+      return this.search(node.right, data);
+    } else {
+      return node;
+    }
+
   }
 
   remove(/* data */) {
